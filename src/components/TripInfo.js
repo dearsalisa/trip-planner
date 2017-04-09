1db7d2ea88344cf8d1e5029b2256562168324e7e
dearsalisa <dearsalisa@gmail.com>
2017-04-10 00:22:59 +0700

    show trip info on header

--- src/components/TripInfo.js
+++ src/components/TripInfo.js
@@ -2,12 +2,11 @@ import React from 'react';
 import '../css/Timeline.css';
 
 const TripInfo = ({tripInfo}) => {
-	//console.log(data)
 	return (
 		<div className="trip_header">
 			<center>
-				<h2>{tripInfo[1]}</h2>
-				<h4>{tripInfo[2]}</h4>
+				<h2>{tripInfo.name}</h2>
+				<h4>{tripInfo.detail}</h4>
 			</center>
 		</div>
 	)
