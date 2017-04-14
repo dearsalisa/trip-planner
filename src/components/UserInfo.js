67d268a49948ee16ada26ad19601f8673b5b8785
dearsalisa <dearsalisa@gmail.com>
2017-04-14 13:02:12 +0700

    add like&share button

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import '../css/Profile.css';
-import { Image } from 'react-bootstrap'
+import { Image, Button, Glyphicon } from 'react-bootstrap'
 
 const UserInfo = (user) => {
 	return (
 		<div className="bg_userinfo">
 			<Image src={user.photoURL} thumbnail />
 			<h3>{user.displayName}</h3>
-			<h4>{user.email == null ? "" : user.email}</h4>
+			<h5>{user.email == null ? "" : user.email}</h5>
+			<Button className="profile_btn" bsSize="large" >
+				<Glyphicon className="calendar" glyph="calendar" /> 3  TRIPS
+			</Button>
+			<Button className="profile_btn" bsSize="large" >
+				<Glyphicon className="heart" glyph="heart" /> LIKE
+			</Button>
 		</div>
 	)
 }
