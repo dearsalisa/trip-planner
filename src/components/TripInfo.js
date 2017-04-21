88456220361a105058ea89fa0c1c52a62ba5667e
dearsalisa <dearsalisa@gmail.com>
2017-04-21 18:18:40 +0700

    can edit name/detail of trip

--- src/components/TripInfo.js
+++ src/components/TripInfo.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/Timeline.css';
+import { Button, Modal, Glyphicon } from 'react-bootstrap'
 
-const TripInfo = ({tripInfo}) => {
-	return (
-		<div className="tl_header">
-			<div className="tl_info">
-				<h1 className="tl_name">{tripInfo.name}</h1>
-				<h4 className="tl_detail">{tripInfo.detail}</h4>
+class TripInfo extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      showModal: false
+    };
+		this.close = this.close.bind(this)
+		this.open = this.open.bind(this)
+		this.save = this.save.bind(this)
+  }
+
+  save() {
+		this.props.callBack({
+			name : this.refs.name.value,
+			detail : this.refs.detail.value,
+		})
+		this.close()
+  }
+
+  close() { this.setState({ showModal: false }) }
+  open() { this.setState({ showModal: true }) }
+
+  render() {
+    return (
+			<div className="tl_header">
+				<div className="tl_info">
+					<h1 className="tl_name">{this.props.tripInfo.name}</h1>
+					<h4 className="tl_detail">{this.props.tripInfo.detail}</h4>
+					<span onClick={this.open} >
+	          <span className="info_edit"><Glyphicon glyph="edit" /> แก้ไข </span>
+	        </span>
+					<Modal show={this.state.showModal} onHide={this.close}>
+	          <Modal.Header closeButton>
+	            <Modal.Title>Edit Trip Info</Modal.Title>
+	          </Modal.Header>
+	          <Modal.Body>
+	            <form>
+	              <label>Name</label><input ref="name" defaultValue={this.props.tripInfo.name} /><br />
+	              <label>Detail</label><br />
+	              <textarea ref="detail" defaultValue={this.props.tripInfo.detail} rows="5"></textarea><br />
+	            </form>
+	          </Modal.Body>
+	          <Modal.Footer>
+	            <Button onClick={this.save}>Save</Button>
+	            <Button onClick={this.close}>Close</Button>
+	          </Modal.Footer>
+	        </Modal>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default TripInfo
