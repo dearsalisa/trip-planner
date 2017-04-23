7c4a478d3d11cdd7bf0b88dc7b48d39530794420
dearsalisa <dearsalisa@gmail.com>
2017-04-23 22:57:44 +0700

    add map view button to tripview page

--- src/containers/Tripview.jsx
+++ src/containers/Tripview.jsx
@@ -13,7 +13,8 @@ class Tripview extends Component {
       showModal: false,
       trip: trip === undefined ? {} : trip,
       trip_id: props.routeParams.tripKey,
-      tripUrl: this.props.location.pathname
+      tripUrl: this.props.location.pathname,
+      isShowMap: false
     }
     this.close = this.close.bind(this)
     this.open = this.open.bind(this)
@@ -35,47 +36,52 @@ class Tripview extends Component {
   render() {
     return (
       <center className="bg">
-        <div className="page">
+        <div>
           <TripInfoView {...this.state}  />
-            <div className="tripview">
-              {
-                this.state.trip.timeline !== undefined ?
-                this.state.trip.timeline.map(input =>
-                  <div className="day_box" key={input.day}>
-                    <Panel className="day" header={
-                      <div>Day {input.day}</div>
-                    }>
-                    {
-                      this.state.trip.timeline[parseInt(input.day)-1].travel !== undefined ?
-                      this.state.trip.timeline[parseInt(input.day)-1].travel
-                        .sort( (i,j) => { return i.time > j.time})
-                        .map((item,index) =>
-                          <div className="event_box" key={input.day+item.name}>
-                            <div className="name">
-                              <h4><span className="circle"></span><b>{item.time}</b> {item.name} </h4>
-                            </div>
-                            <div className="square">
-                              <div className="trip_text">
-                                <h5>{item.location.name !== undefined ? "Location : "+item.location.name : ""}</h5>
-                                {
-                                  item.image !== undefined ?
-                                  item.image.map((image, index) =>
-                                    <img key={index} className="tl_pic" role="presentation" src={image}/>
-                                  ) : ""
-                                }
-                                <h5 className="detail">{item.detail}</h5>
-                                <a href={item.link}>{item.link}</a>
-                                <h5>{item.mark}</h5>
-                              </div>
+          <div className="tripview">
+            <div className="map_box">
+              <Button className="map_view" bsSize="large" active onClick={ () => {this.setState({isShowMap: !this.state.isShowMap })} }>
+                { this.state.isShowMap ? "TIMELINE VIEW" : "MAP VIEW"}
+              </Button>
+            </div>
+            {
+              this.state.trip.timeline !== undefined ?
+              this.state.trip.timeline.map(input =>
+                <div className="day_box" key={input.day}>
+                  <Panel className="day" header={
+                    <div>Day {input.day}</div>
+                  }>
+                  {
+                    this.state.trip.timeline[parseInt(input.day)-1].travel !== undefined ?
+                    this.state.trip.timeline[parseInt(input.day)-1].travel
+                      .sort( (i,j) => { return i.time > j.time})
+                      .map((item,index) =>
+                        <div className="event_box" key={input.day+item.name}>
+                          <div className="name">
+                            <h4><span className="circle"></span><b>{item.time}</b> {item.name} </h4>
+                          </div>
+                          <div className="square">
+                            <div className="trip_text">
+                              <h5>{item.location.name !== undefined ? "Location : "+item.location.name : ""}</h5>
+                              {
+                                item.image !== undefined ?
+                                item.image.map((image, index) =>
+                                  <img key={index} className="tl_pic" role="presentation" src={image}/>
+                                ) : ""
+                              }
+                              <h5 className="detail">{item.detail}</h5>
+                              <a href={item.link}>{item.link}</a>
+                              <h5>{item.mark}</h5>
                             </div>
                           </div>
-                      ) : <div className="event_form"><h5>no event</h5></div>
-                    }
-                  </Panel>
-                  </div>
-                ) : ""
-              }
-            </div>
+                        </div>
+                    ) : <div className="event_form"><h5>no event</h5></div>
+                  }
+                </Panel>
+                </div>
+              ) : ""
+            }
+          </div>
         </div>
       </center>
     )
